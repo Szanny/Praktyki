59788638ae3058cc99294e734883dc4514c25f13
Szanny <turtezin@gmail.com>
2024-08-07 07:49:55 +0200

    prawie zrobione

---
+++ DataViewGrid/DataViewGrid/Form1.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace DataViewGrid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 411);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ DataViewGrid/DataViewGrid/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataViewGrid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Podstawa();
+            Suma();
+        }
+        private void Podstawa()
+        {
+            dataGridView1.ColumnCount = 4;
+            dataGridView1.Columns[0].Name = "A";
+            dataGridView1.Columns[1].Name = "B";
+            dataGridView1.Columns[2].Name = "C";
+            dataGridView1.Columns[3].Name = "D";
+
+            for (int i = 0; i < 4; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+
+            dataGridView1.Rows[0].DefaultCellStyle.BackColor = System.Drawing.Color.Gray;
+            dataGridView1.Rows[0].ReadOnly = true;
+
+            dataGridView1.CellValueChanged += dataGridView1_CellValueChanged;
+        }
+        private void Suma()
+        {
+            for (int c = 0; c < dataGridView1.ColumnCount; c++)
+            {
+                int sum = 0;
+                for(int r = 1; r < dataGridView1.RowCount; r++)
+                {
+                    if (dataGridView1.Rows[r].Cells[c].Value != null)
+                    {
+                        if (int.TryParse(dataGridView1.Rows[r].Cells[c].Value.ToString(), out int liczba))
+                        {
+                            sum += liczba;
+                        }
+                    }
+                }
+                //if sum > 1000 blad ma sie wyswietlic
+                dataGridView1.Rows[0].Cells[c].Value = sum;
+            }
+        }
+        public void Okno()
+        {
+            Form prompt = new Form();
+            {
+
+            };
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+        private void dataGridView1_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex > 0)
+            {
+                Suma();
+            }
+        }
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
